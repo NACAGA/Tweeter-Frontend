9fe0fb6c11a22e02d3d4b29beb72ff062b9de70a
Nate-Louder <nmlouder@gmail.com>
2024-02-29 18:22:29 -0500

    Added create group modal

--- src/lib/components/sidebar/Sidebar.tsx
+++ src/lib/components/sidebar/Sidebar.tsx
@@ -13,10 +13,17 @@ import MailIcon from '@mui/icons-material/Mail';
 import SidebarLink from './SidebarLink';
 import SidebarHeader from './SidebarHeader';
 import './Sidebar.scss';
+import SidebarAction from './SidebarAction';
+import NewGroupModal from '../new-group/NewGroupModal';
 
 function Sidebar() {
     const [closed, setClosed] = useState(false);
+    const [createGroupModalOpen, setCreateGroupModalOpen] = useState(false);
     const sidebarWidth = closed ? 88 : 256;
+
+    const toggleCreateGroupModal = () => {
+        setCreateGroupModalOpen((prev) => !prev);
+    };
 
     const handleClick = () => {
         setClosed((prev) => !prev);
@@ -28,9 +35,10 @@ function Sidebar() {
             <SidebarLink href="/" title="Home" icon="home" iconOnly={closed} />
             <SidebarLink href="/join-groups" title="Join Groups" icon="search" iconOnly={closed} />
             <SidebarLink href="/my-groups" title="My Groups" icon="group" iconOnly={closed} />
-            <SidebarLink href="/creategroup" title="Create Group" icon="addCircle" iconOnly={closed} />
-            <SidebarLink href="/newpost" title="New Post" icon="addPage" iconOnly={closed} />
+            <SidebarAction title="Create Group" icon="addCircle" iconOnly={closed} onClick={toggleCreateGroupModal} />
+            <SidebarAction title="New Post" icon="addPage" iconOnly={closed} />
             <SidebarLink href="/profile" title="Profile" icon="person" iconOnly={closed} />
+            <NewGroupModal userid={1} open={createGroupModalOpen} onClose={toggleCreateGroupModal} />
         </div>
     );
 }
--- src/lib/components/sidebar/SidebarAction.tsx
+++ src/lib/components/sidebar/SidebarAction.tsx
@@ -1,1 +1,36 @@
-export {};
+import React, { ReactElement } from 'react';
+import { IconName, IconNameEnum } from 'lib/types/enums/IconNameEnum';
+import './Sidebar.scss';
+import { Link, NavLink } from 'react-router-dom';
+import { Button, Typography } from '@mui/material';
+
+interface Props {
+    onClick?: () => void;
+    title: string;
+    icon: IconName;
+    iconOnly?: boolean;
+}
+
+function SidebarAction(props: Props): ReactElement {
+    return (
+        <Button
+            onClick={props.onClick}
+            sx={{
+                justifyContent: 'start',
+                width: '100%',
+                height: 'fit-content',
+                fontSize: '20px',
+                textDecoration: 'none',
+                padding: '0px 2px',
+                textTransform: 'capitalize',
+            }}
+        >
+            <div className={`sidebar-link__container ${props.iconOnly ? 'sidebar-link__container--closed' : ''}`}>
+                <span className="sidebar-link__icon">{IconNameEnum[props.icon]}</span>
+                {props.iconOnly ? '' : <span className="sidebar-link__title">{props.title}</span>}
+            </div>
+        </Button>
+    );
+}
+
+export default SidebarAction;
